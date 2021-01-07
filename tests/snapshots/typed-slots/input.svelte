f17a914f1e73d13a52812900aa70530391a99d7b
Eric Liu <ericyl.us@gmail.com>
2021-01-07 12:00:24 -0800

    test: add typed slots test

---
+++ tests/snapshots/typed-slots/input.svelte
@@ -0,0 +1,16 @@
+<script>
+  /**
+   * @slot {{ prop: number; doubled: number; }}
+   * @slot {{ props: { class?: string; } }} description
+   */
+
+  export let prop = 0;
+</script>
+
+<h1>
+  <slot {prop} doubled={prop * 2} />
+</h1>
+
+<p>
+  <slot name="description" props={{ class: $$props.class }} />
+</p>
