21a9e9fb48e36ff9edfe600e70f387077a77fed3
Eric Liu <ericyl.us@gmail.com>
2021-01-09 14:48:04 -0800

    refactor(writer-json): normalize component filePath, sort by module name

--- src/writer/writer-json.ts
+++ src/writer/writer-json.ts
@@ -1,5 +1,4 @@
 import * as path from "path";
-import * as fs from "fs";
 import { ComponentDocApi, ComponentDocs } from "../rollup-plugin";
 import Writer from "./Writer";
 
@@ -14,13 +13,16 @@ interface JsonOutput {
 }
 
 export default async function writeJson(components: ComponentDocs, options: WriteJsonOptions) {
-  const file_path = fs.lstatSync(options.input).isDirectory() ? options.input : path.dirname(options.input);
   const output: JsonOutput = {
     total: components.size,
     components: Array.from(components, ([moduleName, component]) => ({
       ...component,
-      filePath: path.join("/", file_path, component.filePath),
-    })),
+      filePath: path.normalize(component.filePath),
+    })).sort((a, b) => {
+      if (a.moduleName < b.moduleName) return -1;
+      if (a.moduleName > b.moduleName) return 1;
+      return 0;
+    }),
   };
 
   const output_path = path.join(process.cwd(), options.outFile);
