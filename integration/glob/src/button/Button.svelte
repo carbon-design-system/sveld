955845c3bd20eb65dd7cf9d1cb92df869edce48d
Neel <neelneelneel@users.noreply.github.com>
2021-02-20 08:07:53 -0500

    Fixing error when there are non svelte files (#26)
    * Fixing error when there are non svelte files
    
    * Apply suggestions from code review
    
    Co-authored-by: Eric Liu <ericyl.us@gmail.com>
    
    Co-authored-by: neelneelneel <>
    Co-authored-by: Eric Liu <ericyl.us@gmail.com>

---
+++ integration/glob/src/button/Button.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let type = "button2";
+  export let primary = false;
+</script>
+
+<button {...$$restProps} {type} class:primary on:click>
+  <slot>Click me</slot>
+</button>
---
+++ integration/glob/src/index.js
@@ -0,0 +1,2 @@
+export { Action } from "./action/";
+export { Button } from "./button/";
