181a4f849202d7d709dd2fa0184ed31483d54989
Eric Liu <ericyl.us@gmail.com>
2021-08-28 19:21:57 -0700

    test(integration): add test for glob

---
+++ integration/glob/test/test.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+import { Button } from "../types"
+import Button as Button2 from "../types/button/Button.svelte";
+</script>
+
+<Button primary="{true}">Test</Button>
+<Button2 primary="{true}">Test</Button2>
