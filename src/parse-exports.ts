d39e228debff747501acf2598adaa60da77c5244
Ethan Jones <ethan.jones@vizio.com>
2022-05-05 11:44:14 -0600

    Support Export all for component parsing in svelte entrypoint

--- src/parse-exports.ts
+++ src/parse-exports.ts
@@ -1,4 +1,6 @@
 import * as acorn from "acorn";
+import * as fs from "fs";
+import path from "path";
 
 interface NodeImportDeclaration extends acorn.Node {
   type: "ImportDeclaration";
@@ -16,11 +18,15 @@ interface NodeExportDefaultDeclaration extends acorn.Node {
   declaration: { name: string };
 }
 
-type BodyNode = NodeImportDeclaration | NodeExportNamedDeclaration | NodeExportDefaultDeclaration;
+interface NodeExportAllDeclaration extends acorn.Node, Pick<NodeImportDeclaration, "source"> {
+  type: "ExportAllDeclaration";
+}
+
+type BodyNode = NodeImportDeclaration | NodeExportNamedDeclaration | NodeExportDefaultDeclaration | NodeExportAllDeclaration;
 
 export type ParsedExports = Record<string, { source: string; default: boolean; mixed?: boolean }>;
 
-export function parseExports(source: string) {
+export function parseExports(source: string, dir: string) {
   const ast = acorn.parse(source, {
     ecmaVersion: "latest",
     sourceType: "module",
@@ -38,9 +44,26 @@ export function parseExports(source: string) {
       } else {
         exports_by_identifier[id] = { source: "", default: true };
       }
-    }
+    } else if (node.type === "ExportAllDeclaration") {
+      if (!node.source) return;
 
-    if (node.type === "ExportNamedDeclaration") {
+      let file_path = path.resolve(dir, node.source.value);
+
+      if (!fs.lstatSync(file_path).isFile()){
+        const files = fs.readdirSync(file_path);
+
+        for (const file of files)
+          if (file.includes("index")) {
+            file_path = path.join(file_path, file);
+            break;
+          }
+      }
+
+      const export_file = fs.readFileSync(file_path, "utf-8");
+      const exports = parseExports(export_file, path.dirname(file_path));
+
+      for (const [key, value] of Object.entries(exports)) exports_by_identifier[key] = value;
+    } else if (node.type === "ExportNamedDeclaration") {
       node.specifiers.forEach((specifier) => {
         const exported_name = specifier.exported.name;
         const id = exported_name || specifier.local.name;
