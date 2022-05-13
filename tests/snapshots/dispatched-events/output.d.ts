83f6e0338fe46ecb108a57d98ea32216e6637868
Eric Liu <ericyl.us@gmail.com>
2022-05-13 09:08:45 -0700

    Run "yarn test:snapshot"

--- tests/snapshots/dispatched-events/output.d.ts
+++ tests/snapshots/dispatched-events/output.d.ts
@@ -7,9 +7,9 @@ export default class Input extends SvelteComponentTyped<
   InputProps,
   {
     hover: CustomEvent<any>;
-    destroy: CustomEvent<any>;
-    ["destroy--component"]: CustomEvent<any>;
-    ["destroy:component"]: CustomEvent<any>;
+    destroy: CustomEvent<null>;
+    ["destroy--component"]: CustomEvent<null>;
+    ["destroy:component"]: CustomEvent<null>;
   },
   { default: {} }
 > {}
