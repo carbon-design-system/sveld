e8f6ea3d2b4e0815f688a11add0c5901cdbfc8e4
Eric Liu <ericyl.us@gmail.com>
2022-05-19 05:59:55 -0700

    test(snapshots): add required props test case

---
+++ tests/snapshots/required/input.svelte
@@ -0,0 +1,15 @@
+<script>
+  /**
+   * @required
+   */
+  export let prop = true;
+
+  /**
+   * This is a comment.
+   * @required
+   * @type {boolean | string}
+   */
+  export let prop1 = true;
+</script>
+
+<slot {prop} {prop1} />
