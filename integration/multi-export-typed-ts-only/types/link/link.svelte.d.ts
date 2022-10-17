2689619e775166e8d889da14205ff477f1469a62
Eric Liu <ericyl.us@gmail.com>
2022-10-17 09:38:25 -0700

    Run "yarn prepack; yarn test:integration"

--- integration/multi-export-typed-ts-only/types/link/link.svelte.d.ts
+++ integration/multi-export-typed-ts-only/types/link/link.svelte.d.ts
@@ -2,31 +2,7 @@
 import type { SvelteComponentTyped } from "svelte";
 
 export interface LinkProps
-  extends svelte.JSX.HTMLAttributes<HTMLElementTagNameMap["a"]> {
-  /**
-   * SvelteKit attribute to enable data prefetching
-   * if a link is hovered over or touched on mobile.
-   * @see https://kit.svelte.dev/docs/a-options#sveltekit-prefetch
-   * @default false
-   */
-  "sveltekit:prefetch"?: boolean;
-
-  /**
-   * SvelteKit attribute to trigger a full page
-   * reload after the link is clicked.
-   * @see https://kit.svelte.dev/docs/a-options#sveltekit-reload
-   * @default false
-   */
-  "sveltekit:reload"?: boolean;
-
-  /**
-   * SvelteKit attribute to prevent scrolling
-   * after the link is clicked.
-   * @see https://kit.svelte.dev/docs/a-options#sveltekit-noscroll
-   * @default false
-   */
-  "sveltekit:noscroll"?: boolean;
-}
+  extends svelte.JSX.HTMLAttributes<HTMLElementTagNameMap["a"]> {}
 
 export default class Link extends SvelteComponentTyped<
   LinkProps,
--- tests/snapshots/anchor-props/output.d.ts
+++ tests/snapshots/anchor-props/output.d.ts
@@ -1,30 +1,6 @@
 /// <reference types="svelte" />
 import type { SvelteComponentTyped } from "svelte";
 
-export interface InputProps extends svelte.JSX.HTMLAttributes<HTMLElementTagNameMap["a"]> {
-  /**
-   * SvelteKit attribute to enable data prefetching
-   * if a link is hovered over or touched on mobile.
-   * @see https://kit.svelte.dev/docs/a-options#sveltekit-prefetch
-   * @default false
-   */
-  "sveltekit:prefetch"?: boolean;
-
-  /**
-   * SvelteKit attribute to trigger a full page
-   * reload after the link is clicked.
-   * @see https://kit.svelte.dev/docs/a-options#sveltekit-reload
-   * @default false
-   */
-  "sveltekit:reload"?: boolean;
-
-  /**
-   * SvelteKit attribute to prevent scrolling
-   * after the link is clicked.
-   * @see https://kit.svelte.dev/docs/a-options#sveltekit-noscroll
-   * @default false
-   */
-  "sveltekit:noscroll"?: boolean;
-}
+export interface InputProps extends svelte.JSX.HTMLAttributes<HTMLElementTagNameMap["a"]> {}
 
 export default class Input extends SvelteComponentTyped<InputProps, {}, { default: {} }> {}
