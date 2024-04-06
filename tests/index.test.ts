61d6589bea25109b4c4f1eb42f9baa7a7b3a5ac8
Eric Liu <ericyl.us@gmail.com>
2024-04-06 09:34:54 -0700

    test: remove smoke test

--- tests/index.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "vitest";
-import * as API from "../src";
-
-test("Library exports", () => {
-  expect(Object.keys(API)).toEqual(["default", "ComponentParser", "cli", "sveld"]);
-});
