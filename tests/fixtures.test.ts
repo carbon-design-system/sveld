dbbc0c2b6d65385087b157fe0740165cdc1b5d9f
Eric Liu <ericyl.us@gmail.com>
2024-04-06 16:49:10 -0700

    test: update fixture snapshots

--- tests/fixtures.test.ts
+++ tests/fixtures.test.ts
@@ -1,32 +1,31 @@
-import fg from "fast-glob";
-import fs from "node:fs";
-import fsp from "node:fs/promises";
+import { Glob } from "bun";
 import path from "node:path";
 import ComponentParser from "../src/ComponentParser";
 import Writer from "../src/writer/Writer";
 import { writeTsDefinition } from "../src/writer/writer-ts-definitions";
 
 const folder = path.join(process.cwd(), "tests", "fixtures");
-const svelteFiles = fg.sync(["**/*.svelte"], { cwd: folder });
-const fixtures = svelteFiles.map((file) => {
-  return {
-    path: file,
-    source: fs.readFileSync(path.join(folder, file), "utf-8"),
-  };
-});
+const fixtures_map = new Map<string, string>();
+
+for await (const file of new Glob("**/*.svelte").scan(folder)) {
+  const source = await Bun.file(path.join(folder, file)).text();
+  fixtures_map.set(file, source);
+}
 
 const parser = new ComponentParser();
 const writer = new Writer({ parser: "typescript", printWidth: 120 });
+const files = Array.from(fixtures_map.keys());
 
-const getMetadata = (fixture: (typeof fixtures)[0]) => {
-  const { dir } = path.parse(fixture.path);
+const getMetadata = (fixture: { filePath: string; source: string }) => {
+  const { filePath, source } = fixture;
+  const { dir } = path.parse(filePath);
   const moduleName = dir
     .split("-")
     .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
     .join("");
-  const metadata = { moduleName, filePath: fixture.path };
-  const parsed_component = parser.parseSvelteComponent(fixture.source, {
-    filePath: fixture.path,
+  const metadata = { moduleName, filePath };
+  const parsed_component = parser.parseSvelteComponent(source, {
+    filePath,
     moduleName,
   });
 
@@ -34,27 +33,33 @@ const getMetadata = (fixture: (typeof fixtures)[0]) => {
 };
 
 describe("fixtures (JSON)", async () => {
-  test.concurrent.each(fixtures)("$path", async (fixture) => {
-    const { dir, parsed_component } = getMetadata(fixture);
+  test.each(files)("%p", async (filePath) => {
+    const { dir, parsed_component } = getMetadata({
+      filePath,
+      source: fixtures_map.get(filePath)!,
+    });
     const api_json = JSON.stringify(parsed_component, null, 2);
 
     // Snapshot the output; if the test fails, output has changed.
     expect(api_json).toMatchSnapshot();
 
     // Still write to disk to manually assert types as needed.
-    await fsp.writeFile(path.join(folder, dir, "output.json"), api_json);
+    await Bun.write(path.join(folder, dir, "output.json"), api_json);
   });
 });
 
 describe("fixtures (TypeScript)", async () => {
-  test.concurrent.each(fixtures)("$path", async (fixture) => {
-    const { dir, metadata, parsed_component } = getMetadata(fixture);
+  test.each(files)("%p", async (filePath) => {
+    const { dir, metadata, parsed_component } = getMetadata({
+      filePath,
+      source: fixtures_map.get(filePath)!,
+    });
     const api_ts = writer.format(writeTsDefinition({ ...metadata, ...parsed_component }));
 
     // Snapshot the output; if the test fails, output has changed.
     expect(api_ts).toMatchSnapshot();
 
     // Still write to disk to manually assert types as needed.
-    await fsp.writeFile(path.join(folder, dir, "output.d.ts"), api_ts);
+    await Bun.write(path.join(folder, dir, "output.d.ts"), api_ts);
   });
 });
