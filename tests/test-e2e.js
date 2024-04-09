67147fe4586f9d9a40df89aa80cb53fa9ec53866
metonym <ericyl.us@gmail.com>
2024-04-08 20:47:19 -0700

    chore: add e2e tests to test workflow

--- tests/test-e2e.js
+++ tests/test-e2e.js
@@ -1,21 +1,20 @@
 const fs = require("fs");
-const fsp = require("fs/promises");
 const path = require("path");
 const { exec: child_process_exec } = require("child_process");
 const { promisify } = require("util");
 const exec = promisify(child_process_exec);
 const { name } = require("../package.json");
 
-const dirs = fs
-  .readdirSync("integration")
-  .map((i) => path.join("integration", i))
-  .filter((i) => fs.lstatSync(i).isDirectory());
-
 const execCwd = async (dir, ...args) => await exec(`yarn --cwd ${dir} ${args}`);
 
 (async () => {
   try {
     await exec("yarn link");
+
+    const dirs = fs
+      .readdirSync("tests/e2e")
+      .map((file) => path.join("tests/e2e", file))
+      .filter((file) => fs.lstatSync(file).isDirectory());
 
     for await (const dir of dirs) {
       const typesDir = path.join(dir, "types");
