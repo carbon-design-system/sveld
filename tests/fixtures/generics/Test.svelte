005b48aa4bc473d98ccaf0f100a94c9e3146d973
Eric Liu <ericyl.us@gmail.com>
2024-04-20 13:59:09 -0700

    test: add generics fixture

---
+++ tests/fixtures/generics/Test.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Generics from "./output";
+</script>
+
+<Generics
+  headers={[
+    {
+      key: "name",
+      value: "Name",
+    },
+    {
+      key: "port",
+      value: "Port",
+    },
+  ]}
+  rows={[
+    { id: 1, name: "Name 1", port: 3000 },
+    { id: 2, name: "Name 2", port: 3000 },
+    { id: 3, name: "Name 3", port: 3000 },
+  ]}
+  let:rows
+>
+  {#each rows as row}
+    {row.name}
+  {/each}
+</Generics>
---
+++ tests/fixtures/generics/input.svelte
@@ -0,0 +1,17 @@
+<script>
+  /**
+   * @typedef {{ id: string | number; [key: string]: any; }} DataTableRow
+   * @typedef {Exclude<keyof Row, "id">} DataTableKey<Row>
+   * @typedef {{ key: DataTableKey<Row>; value: string; }} DataTableHeader<Row>
+   * @template {DataTableRow} Row
+   * @generics {Row extends DataTableRow = DataTableRow} Row
+   */
+
+  /** @type {ReadonlyArray<DataTableHeader<Row>>} */
+   export let headers = [];
+
+  /** @type {ReadonlyArray<Row>} */
+  export let rows = [];
+</script>
+
+<slot {rows} />
