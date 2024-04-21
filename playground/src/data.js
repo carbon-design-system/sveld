50c5965240a6db5c363a34898f6707cd383d16a9
Eric Liu <ericyl.us@gmail.com>
2024-04-21 09:08:23 -0700

    chore(playground): add generics example

--- playground/src/data.js
+++ playground/src/data.js
@@ -44,4 +44,26 @@ export const forwarded_events = {
 `,
 };
 
-export default [button, dynamic_dispatched_events, forwarded_events];
+export const generics = {
+  moduleName: "Generics",
+  code: `<script>
+  /**
+   * @typedef {{ id: string | number; [key: string]: any; }} DataTableRow
+   * @typedef {Exclude<keyof Row, "id">} DataTableKey<Row>
+   * @typedef {{ key: DataTableKey<Row>; value: string; }} DataTableHeader<Row=DataTableRow>
+   * @template {DataTableRow} <Row extends DataTableRow = DataTableRow>
+   * @generics {Row extends DataTableRow = DataTableRow} Row
+   */
+
+  /** @type {ReadonlyArray<DataTableHeader<Row>>} */
+   export let headers = [];
+
+  /** @type {ReadonlyArray<Row>} */
+  export let rows = [];
+</script>
+
+<slot {headers} {rows} />
+`,
+};
+
+export default [button, dynamic_dispatched_events, forwarded_events, generics];
