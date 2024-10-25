3826acbacd524cc3489bb590b41baf2f01a04d3e
Eric Liu <ericyl.us@gmail.com>
2024-10-25 11:51:36 -0700

    test(fixtures): fix `generics-multiple` example (#141)

--- tests/fixtures/generics-multiple/input.svelte
+++ tests/fixtures/generics-multiple/input.svelte
@@ -2,9 +2,9 @@
   /**
    * @typedef {{ id: string | number; [key: string]: any; }} DataTableRow
    * @typedef {Exclude<keyof Row, "id">} DataTableKey<Row>
-   * @typedef {{ key: DataTableKey<Row>; value: Header; }} DataTableHeader<Row=DataTableRow,Header>
+   * @typedef {{ key: DataTableKey<Row>; value: Header; }} DataTableHeader<Row=DataTableRow,Header=DataTableRow>
    * @template {DataTableRow} <Row extends DataTableRow = DataTableRow>
-   * @generics {Row extends DataTableRow = DataTableRow, Header extends DataTableRow} Row,Header
+   * @generics {Row extends DataTableRow = DataTableRow, Header extends DataTableRow = DataTableRow} Row,Header
    */
 
   /** @type {ReadonlyArray<DataTableHeader<Row, Header>>} */
