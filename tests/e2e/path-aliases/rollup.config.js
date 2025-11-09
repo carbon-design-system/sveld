e917537cb60bca25df62a52c5c8340ef1e8cfadc
Eric Liu <ericyl.us@gmail.com>
2025-11-09 09:56:52 -0800

    test(e2e): fix path-aliases test

---
+++ tests/e2e/path-aliases/rollup.config.js
@@ -0,0 +1,27 @@
+import pkg from "./package.json";
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import sveld from "sveld";
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: pkg.svelte,
+  output: { format: "es", file: pkg.module },
+  plugins: [
+    svelte(),
+    resolve(),
+    production &&
+      sveld({
+        markdown: true,
+        markdownOptions: {
+          onAppend: (type, document, components) => {
+            if (type === "h1")
+              document.append("quote", `${components.size} components exported from ${pkg.name}@${pkg.version}.`);
+          },
+        },
+        json: true,
+      }),
+  ],
+};
+
