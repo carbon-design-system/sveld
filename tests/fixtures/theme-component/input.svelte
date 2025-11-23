d00474d40111275701578a01460b75bd8204b5ff
Eric Liu <ericyl.us@gmail.com>
2025-11-23 10:32:43 -0800

    fix(writer): add line break after `export declare const`
    Improves visual separation between module-level `const` exports
    and subsequent type definitions by appending a newline after
    `export declare const` statements in generated `.d.ts` files.

---
+++ tests/fixtures/theme-component/input.svelte
@@ -0,0 +1,45 @@
+<script context="module">
+  /** @type {Record<CarbonTheme, string>} */
+  export const themes = {
+    white: "White",
+    g10: "Gray 10",
+    g80: "Gray 80",
+    g90: "Gray 90",
+    g100: "Gray 100",
+  };
+</script>
+
+<script>
+  /**
+   * Dynamic, client-side theming using CSS variables
+   */
+
+  /**
+   * @typedef {"white" | "g10" | "g80" | "g90" | "g100"} CarbonTheme
+   * @event update
+   * @type {object}
+   * @property {CarbonTheme} theme
+   * @slot {{ theme: CarbonTheme; }}
+   */
+
+  /**
+   * Set the current Carbon theme
+   * @type {CarbonTheme}
+   */
+  export let theme = "white";
+
+  /**
+   * Customize a theme with your own tokens
+   * @see https://carbondesignsystem.com/guidelines/themes/overview#customizing-a-theme
+   * @type {{ [token: string]: any; }}
+   */
+  export let tokens = {};
+
+  /** Set to `true` to persist the theme using window.localStorage */
+  export let persist = false;
+
+  /** Specify the local storage key */
+  export let persistKey = "theme";
+</script>
+
+<slot {theme} />
---
+++ tests/fixtures/theme-component/output.d.ts
@@ -0,0 +1,38 @@
+import type { SvelteComponentTyped } from "svelte";
+
+export declare const themes: Record<CarbonTheme, string>;
+
+export type CarbonTheme = "white" | "g10" | "g80" | "g90" | "g100";
+
+export type ThemeComponentProps = {
+  /**
+   * Set the current Carbon theme
+   * @default "white"
+   */
+  theme?: CarbonTheme;
+
+  /**
+   * Customize a theme with your own tokens
+   * @see https://carbondesignsystem.com/guidelines/themes/overview#customizing-a-theme
+   * @default {}
+   */
+  tokens?: { [token: string]: any };
+
+  /**
+   * Set to `true` to persist the theme using window.localStorage
+   * @default false
+   */
+  persist?: boolean;
+
+  /**
+   * Specify the local storage key
+   * @default "theme"
+   */
+  persistKey?: string;
+};
+
+export default class ThemeComponent extends SvelteComponentTyped<
+  ThemeComponentProps,
+  { update: CustomEvent<{ theme: CarbonTheme }> },
+  { default: { theme: CarbonTheme } }
+> {}
