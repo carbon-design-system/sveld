1401a6018463741b241eff7b1c1500eaa34e34e2
Eric Liu <ericyl.us@gmail.com>
2026-01-17 10:25:23 -0800

    test(fixtures): add complex prop comments case

---
+++ tests/fixtures/prop-comment-complex/input.svelte
@@ -0,0 +1,29 @@
+<script>
+  /**
+   * Specify the switch text.
+   * Alternatively, use the default slot.
+   * @example
+   * ```svelte
+   * <Switch>
+   *   <span>Custom Text</span>
+   * </Switch>
+   * ```
+   */
+  export let text = "";
+
+  /**
+   * Specify the icon to render.
+   * Alternatively, use the named slot "icon".
+   *
+   * @type {any}
+   * @example
+   * ```svelte
+   * <Button>
+   *   <Icon slot="icon" size={20} />
+   * </Button>
+   * ```
+   */
+  export let icon = undefined;
+</script>
+
+<slot {text} {icon} />
