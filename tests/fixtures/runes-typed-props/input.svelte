1f6582875e6bafe6dfd716531345a6d6d56bfe2d
Eric Liu <ericyl.us@gmail.com>
2026-03-28 16:49:38 -0700

    test(fixtures): add more Runes-only test cases

---
+++ tests/fixtures/runes-typed-props/input.svelte
@@ -0,0 +1,22 @@
+<script>
+  /**
+   * This is a comment.
+   * @see https://github.com/
+   * @deprecated this prop will be removed in the next major release.
+   * @type {boolean | string}
+   */
+  let {
+    prop = true,
+    /** @see https://github.com/ */
+    prop1 = true,
+    /**
+     * This is a comment.
+     * @type {boolean | string}
+     */
+    prop2 = true,
+  } = $props();
+</script>
+
+{prop}
+{prop1}
+{prop2}
