62ab6d33c40dcfcfe765ed8f6dbfb4289349cb41
Eric Liu <ericyl.us@gmail.com>
2026-04-04 12:29:25 -0700

    build(playground): remove `vitePreprocess()`

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,11 +1,11 @@
-import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { optimizeCss, optimizeImports } from "carbon-preprocess-svelte";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     svelte({
-      preprocess: [vitePreprocess(), optimizeImports()],
+      preprocess: [optimizeImports()],
     }),
     optimizeCss(),
   ],
