22eff8a5fcc488e846f99ac3c49611d591449bc0
Eric Liu <ericyl.us@gmail.com>
2026-08-25 12:29:45 -0700

    chore(playground): bump deps

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,7 +3,7 @@ import { optimizeCss, optimizeImports } from "carbon-preprocess-svelte";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [svelte({ preprocess: [optimizeImports()] }), optimizeCss({ experimental: { strict: true } })],
+  plugins: [svelte({ preprocess: [optimizeImports()] }), optimizeCss()],
   optimizeDeps: {
     exclude: ["carbon-components-svelte", "carbon-icons-svelte"],
   },
